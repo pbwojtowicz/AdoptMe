f915c8c73aeb7f1185374b53965e42da4c67ad74
Patrycja Wójtowicz <pb.wojtowicz@gmail.com>
2022-08-09 15:06:18 +0200

    Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ if("serviceWorker" in navigator){
         console.log(error);
     });
 }
+
+function initMap() {
+    // The location of the shelter
+    const shelter = { lat: 50.04588050382137, lng: 19.883301368872996 };
+
+    // The map, centered at zthe shelter
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: shelter,
+   });
+    // The marker, positioned at the shelter
+    const marker = new google.maps.Marker({
+     position: shelter,
+     map: map,
+   });
+ }
+
+  window.initMap = initMap;
